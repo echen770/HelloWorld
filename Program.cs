610108002dd56a61be3e5ec381d1954678deb611
echen <echen@OPVR>
2022-07-06 22:51:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            System.Console.WriteLine(number);
+        }
+    }
+}
